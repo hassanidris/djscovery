b2e1567bf559d9a812a866a4a0b1f051a1299691
Hassen Ahmed <hassan.creative@gmail.com>
2024-09-15 19:24:38 +0200

    footer fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+    <div className="w-full bg-white px-5 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
       copyright
     </div>
   );
