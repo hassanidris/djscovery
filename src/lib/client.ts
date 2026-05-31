bff1c6e32d1548b19ea17f9369131c91605d5d17
Hassen Ahmed <hassan.creative@gmail.com>
2026-05-31 15:32:54 +0200

    upgrading prisma from 5 to latest (7)

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,8 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+function createPrismaClient() {
+  const adapter = new PrismaNeon({
+    connectionString: process.env.POSTGRES_PRISMA_URL!,
+  });
+  return new PrismaClient({ adapter });
+}
+
+const prisma = globalForPrisma.prisma || createPrismaClient();
 
 export default prisma;
 
